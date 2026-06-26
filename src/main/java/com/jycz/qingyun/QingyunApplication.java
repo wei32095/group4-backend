58d1e089c2d6fb7cefffacc5b2c4361f8f139162
Onlyyy111 <1614198574@qq.com>
2026-06-26 13:11:54 +0800

    后端基本架构

---
+++ src/main/java/com/jycz/qingyun/QingyunApplication.java
@@ -0,0 +1,13 @@
+package com.jycz.qingyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QingyunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QingyunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jycz/qingyun/QingyunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jycz.qingyun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QingyunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
