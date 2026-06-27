feefaa9b51fa0ff77f1e2d2b218d5fbb6b8a0147
Onlyyy111 <1614198574@qq.com>
2026-06-27 13:40:19 +0800

    修改个人信息

--- src/main/java/com/jycz/qingyun/service/UserService.java
+++ src/main/java/com/jycz/qingyun/service/UserService.java
@@ -1,6 +1,7 @@
 package com.jycz.qingyun.service;
 
 import com.jycz.qingyun.model.dto.ApiResult;
+import com.jycz.qingyun.model.dto.InfoUpdateRequest;
 import com.jycz.qingyun.model.dto.LoginRequest;
 import com.jycz.qingyun.model.vo.LoginVO;
 import com.jycz.qingyun.model.vo.StudentInfoVO;
@@ -16,4 +17,9 @@ public interface UserService {
      * 获取当前登录用户信息
      */
     ApiResult<StudentInfoVO> getUserInfo(Long userId);
+
+    /**
+     * 修改个人信息
+     */
+    ApiResult<Boolean> updateUserInfo(Long userId, InfoUpdateRequest request);
 }
