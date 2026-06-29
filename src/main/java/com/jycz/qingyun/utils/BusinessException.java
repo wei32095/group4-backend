d943426f71b52e41e2faa83b44433853abcbb037
w9x4y5 <3970449243@qq.com>
2026-06-29 15:59:45 +0800

    自定义业务异常与全局异常处理器

--- src/main/java/com/jycz/qingyun/utils/BusinessException.java
+++ src/main/java/com/jycz/qingyun/utils/BusinessException.java
@@ -4,7 +4,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
-@EqualsAndHashCode(callSuper = true)
+@EqualsAndHashCode(callSuper = true)//
 public class BusinessException extends RuntimeException {
 
     private final Integer code;
